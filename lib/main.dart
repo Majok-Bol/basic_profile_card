3ea275506f6efb581d5c3954ca02ec7474d6bb42
Majok-Bol <bolmajok001@gmail.com>
2025-03-24 12:19:33 +0300

    social link icons

--- lib/main.dart
+++ lib/main.dart
@@ -10,32 +10,69 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Simple Profile Card',style: TextStyle(color: Colors.red,fontSize: 20),),centerTitle: true
+        backgroundColor: Colors.grey,
+        appBar: AppBar(title: Text('Simple Profile Card',style: TextStyle(color: Colors.black,fontWeight:FontWeight.bold,fontSize: 25),),centerTitle: true
+          // ,backgroundColor: Colors.grey
           ,),
         body: Container(
-          padding: EdgeInsets.all(10.0),
-          margin: EdgeInsets.all(10.0),
+          padding: EdgeInsets.all(10),
+          margin:EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: [BoxShadow(color: Colors.grey,blurRadius: 5),]
+          ),
+
           child:Column(
-          children: [
-            CircleAvatar(
-           radius: 100,
-            backgroundImage: AssetImage('assets/images/profile3.webp'),
+
+            children: [
+              SizedBox(
+                child: CircleAvatar(
+                radius: 40,
+                backgroundImage: AssetImage('assets/images/profile3.webp'),
               ),
-            Text('Napoleon Hill'),
-            Text('Android | IOS developer'),
-            Row(
-              children: [
-                Expanded(child: IconButton(onPressed:null, icon:Icon(Icons.facebook))),
-                // Expanded(child: IconButton(onPressed:null, icon:Icon(Icons.twitter))),
-                Expanded(child: IconButton(onPressed:null, icon:Icon(Icons.code))),
-                Expanded(child: IconButton(onPressed:null, icon:Icon(Icons.camera_alt))),
+              ),
+              Text('Majok Bol',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+              Text('Android | IOS developer',),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
 
-              ],
-            )
 
-    ],
+                  IconButton(onPressed:(){
+                    print("Email pressed");
+                  }, icon:Icon(FontAwesomeIcons.envelope),color: Colors.blue,),
+                  Text('bmajk@gmail.com',style: TextStyle(color: Colors.grey),),
+                ],
+              ),
 
-    ),
+
+              SizedBox(height: 6,),
+
+
+              Text('Social links:'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(onPressed:(){
+                    print("Github pressed");
+                  }, icon:Icon(FontAwesomeIcons.github),color: Colors.black,),
+                  IconButton(onPressed:(){
+                    print("Facebook pressed");
+                  }, icon:Icon(FontAwesomeIcons.facebook),color: Colors.blue,),
+                  IconButton(onPressed:(){
+                    print("Instagram pressed");
+                  }, icon:Icon(FontAwesomeIcons.instagram),color: Colors.pink,),
+                  IconButton(onPressed:(){
+                    print("Twitter pressed");
+                  }, icon:Icon(FontAwesomeIcons.twitter),color: Colors.black,),
+                ],
+
+              )
+
+            ],
+          )
+
       ),
     ),
     );
